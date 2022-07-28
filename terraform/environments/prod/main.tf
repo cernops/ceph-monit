a0f182515b5d33f393fd224a7bbdf76ac340ae5c
Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@cern.ch>
2022-07-28 15:45:09 +0200

    project: use gitlab terraform state
    Signed-off-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@cern.ch>

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,15 +1,7 @@
 terraform {
-  backend "s3" {
-    endpoint                    = "https://s3.cern.ch"
-    bucket                      = "ceph-monit-tf-state"
-    key                         = "prod/terraform.tfstate"
-    region                      = "cern"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
+  backend "http" {
   }
 }
-
 module "common" {
   source        = "../../modules/common"
   ES_CEPH_RO_PW = var.ES_CEPH_RO_PW
--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -1,12 +1,5 @@
 terraform {
-  backend "s3" {
-    endpoint                    = "https://s3.cern.ch"
-    bucket                      = "ceph-monit-tf-state"
-    key                         = "qa/terraform.tfstate"
-    region                      = "cern"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
+  backend "http" {
   }
 }
 
